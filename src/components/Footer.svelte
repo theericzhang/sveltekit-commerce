9b7cd6874ecdbbd4f3fd5bd3dbef8049b29823e4
theericzhang <anericzhang@gmail.com>
2022-08-08 21:41:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Footer.svelte
@@ -0,0 +1,3 @@
+<div class="flex w-full flex-col items-center justify-center border-t border-white/30 py-8 px-4">
+  <div>Deployed on <a class="font-bold" href="https://vercel.com/home">▲ Vercel</a></div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<script>
+  import ThreeItemGrid from '$components/ThreeItemGrid.svelte';
+  import Carousel from '$components/Carousel.svelte';
+
+  export let products;
+  $: clothesCollection = products[0]?.node?.products?.edges;
+  $: featuredCollection = products[1]?.node?.products?.edges;
+</script>
+
+<svelte:head>
+  <title>Home – SvelteKit Commerce</title>
+</svelte:head>
+
+<main>
+  <section>
+    <div class="lg:h-[90vh]">
+      <ThreeItemGrid products={featuredCollection} />
+    </div>
+  </section>
+  <section>
+    <Carousel items={clothesCollection} />
+  </section>
+  <section>
+    <div
+      class="flex flex-col border border-black bg-dark py-20 px-8 text-white lg:flex-row lg:items-center"
+    >
+      <div
+        class="mb-4 mr-8 flex-none text-left text-3xl font-black md:text-4xl lg:mb-0 lg:w-1/3 lg:text-right lg:text-6xl"
+      >
+        Dessert dragée halvah croissant.
+      </div>
+      <div>
+        <div class="lg:text-2xl">
+          Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon
+          bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans.
+          Tiramisu sweet roll cheesecake pie carrot cake.
+        </div>
+        <button class="mt-4 font-bold text-svelteOrange hover:text-svelteDark lg:text-2xl">
+          Read it here
+        </button>
+      </div>
+    </div>
+  </section>
+</main>
